c24b1cad3f0c6b1f970de363a1237be804897d17
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-12 22:16:35 -0700

    Initial commit.

---
+++ R/ParquetColumnSeed.R
@@ -0,0 +1,140 @@
+#' Delayed column of a Parquet file
+#'
+#' Represent a column of a Parquet file as a 1-dimensional DelayedArray.
+#' This allows us to use Parquet data inside \linkS4class{DataFrame}s without loading them into memory.
+#'
+#' @param path String containing a path to a Parquet file.
+#' @param column String containing the name of the column inside the file.
+#' @param length Integer containing the number of rows.
+#' If \code{NULL}, this is determined by inspecting the file.
+#' This should only be supplied for efficiency purposes, to avoid a file look-up on construction.
+#' @param type String specifying the type of the data.
+#' If \code{NULL}, this is determined by inspecting the file.
+#' Users may specify this to avoid a look-up, or to coerce the output into a different type.
+#' @param x Either a string containing the path to a Parquet file (to be used as \code{path}),
+#' or an existing ParquetColumnSeed object.
+#' @param ... Further arguments to be passed to the \code{ParquetColumnSeed} constructor.
+#'
+#' @return For \code{ParquetColumnSeed}, a ParquetColumnSeed is returned, obviously.
+#' 
+#' For \code{ParquetColumnVector}, a ParquetColumnVector is returned.
+#'
+#' @author Aaron Lun
+#' 
+#' @examples
+#' # Mocking up a file:
+#' tf <- tempfile()
+#' on.exit(unlink(tf))
+#' arrow::write_parquet(mtcars, tf)
+#'
+#' # Creating a vector:
+#' ParquetColumnVector(tf, column="gear")
+#'
+#' # This happily lives inside DataFrames:
+#' collected <- list()
+#' for (x in colnames(mtcars)) {
+#'     collected[[x]] <- ParquetColumnVector(tf, column=x)
+#' }
+#' DataFrame(collected)
+#'
+#' @aliases
+#' ParquetColumnSeed-class
+#' dim,ParquetColumnSeed-method
+#' type,ParquetColumnSeed-method
+#' extract_array,ParquetColumnSeed-method
+#' ParquetColumnVector-class
+#'
+#' @name ParquetColumnSeed
+NULL
+
+#' @export
+#' @import methods
+setClass("ParquetColumnSeed", slots=c(path="character", column="character", length="integer", type="character"))
+
+#' @export
+setMethod("dim", "ParquetColumnSeed", function(x) x@length)
+
+#' @export
+#' @importFrom DelayedArray type
+setMethod("type", "ParquetColumnSeed", function(x) x@type)
+
+#' @export
+#' @importFrom arrow read_parquet
+#' @importFrom dplyr collect slice_head slice_tail
+setMethod("extract_array", "ParquetColumnSeed", function(x, index) {
+    tab <- read_parquet(x@path, col_select=x@column, as_data_frame=FALSE)
+    slice <- index[[1]]
+
+    if (is.null(slice)) {
+        output <- collect(tab)[[x@column]]
+    } else if (length(slice) == 0) {
+        output <- logical()
+    } else {
+        original <- slice
+        modified <- FALSE
+
+        if (anyDuplicated(slice)) {
+            slice <- unique(slice)
+            modified <- TRUE
+        }
+
+        if (is.unsorted(slice)) {
+            slice <- sort(slice)
+            modified <- TRUE
+        }
+
+        # Find runs of consecutive elements, and batching them together to do
+        # efficient extractions from the Parquet file.
+        delta <- diff(slice)
+        offenders <- which(delta != 1L)
+        starts.i <- c(1L, offenders + 1L)
+        ends.i <- c(offenders, length(slice))
+
+        contents <- vector("list", length(starts.i))
+        for (i in seq_along(starts.i)) {
+            start <- slice[starts.i[i]]
+            end <- slice[ends.i[i]]
+            sliced <- slice_tail(slice_head(tab, n=end), n=end - start + 1L)
+            contents[[i]] <- collect(sliced)[[x@column]]
+        }
+
+        output <- do.call(c, contents)
+        if (modified) {
+            m <- match(original, slice)
+            output <- output[m]
+        }
+    }
+
+    if (!is(output, x@type)) {
+        output <- as(output, x@type)
+    }
+    array(output)
+})
+
+#' @export
+#' @rdname ParquetColumnSeed
+ParquetColumnSeed <- function(path, column, type=NULL, length=NULL) {
+    if (is.null(type) || is.null(length)) {
+        tab <- read_parquet(path, col_select=column, as_data_frame=FALSE)
+        col <- tab[[column]]
+        if (is.null(type)){ 
+            type <- col$type$ToString()
+        }
+        if (is.null(length)) {
+            length <- nrow(tab)
+        }
+    } 
+    new("ParquetColumnSeed", path=path, column=column, length=length, type=type)
+}
+
+#' @export
+setClass("ParquetColumnVector", contains="DelayedArray", slots=c(seed="ParquetColumnSeed"))
+
+#' @export
+#' @rdname ParquetColumnSeed
+ParquetColumnVector <- function(x, ...) {
+    if (!is(x, "ParquetColumnSeed")) {
+        x <- ParquetColumnSeed(x, ...)
+    }
+    DelayedArray(x)
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Bioconductor-compatible Parquet objects
+
+This package implements Bioconductor-friendly bindings to Parquet files so that they can be used inside standard objects like `DataFrame`s and `SummarizedExperiment`s.
